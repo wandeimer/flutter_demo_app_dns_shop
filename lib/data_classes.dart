2c9e73cc2cebd90274b094e53f6322cf93b653c3
Artem Yurchenko <arte_1997@mail.ru>
2020-09-28 15:48:36 +1000

    Connect http request in first page. Need progressBar and await loading data from server

---
+++ lib/data_classes.dart
@@ -0,0 +1,42 @@
+class ScreenArguments {
+  final String name;
+  final String surname;
+  final String mail;
+  final String phone;
+  final String token;
+
+  ScreenArguments(this.name, this.surname, this.mail, this.phone, this.token);
+}
+
+class TokenResponse {
+  int code;
+  String message;
+  String data;
+
+  TokenResponse(this.code, this.message, this.data);
+
+  factory TokenResponse.fromJson(dynamic json) {
+    return TokenResponse(json['code'] as int, json['message'] as String, json['data'] as String);
+  }
+
+  @override
+  String toString() {
+    return '{ ${this.code}, ${this.message}, ${this.data} }';
+  }
+}
+
+class SummaryResponse {
+  int code;
+  String message;
+
+  SummaryResponse(this.code, this.message);
+
+  factory SummaryResponse.fromJson(dynamic json) {
+    return SummaryResponse(json['code'] as int, json['message'] as String);
+  }
+
+  @override
+  String toString() {
+    return '{ ${this.code}, ${this.message}}';
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'data_classes.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,10 +13,8 @@ class MyApp extends StatelessWidget {
         title: 'Navigation with Arguments',
         home: PreRegistrationScreen(),
         routes: {
-          RegistrationScreen.routeName: (context) =>
-              RegistrationScreen(),
-          PreRegistrationScreen.routeName: (context) =>
-              PreRegistrationScreen(),
+          RegistrationScreen.routeName: (context) => RegistrationScreen(),
+          PreRegistrationScreen.routeName: (context) => PreRegistrationScreen(),
         });
   }
 }
@@ -28,10 +30,45 @@ class PreRegistrationScreen extends StatefulWidget {
 
 class PreRegistrationScreenState extends State<PreRegistrationScreen> {
   final _formState = GlobalKey<FormState>();
+  final String _url = 'https://vacancy.dns-shop.ru/api/candidate/token';
+  String _body;
+  int _status;
+  TokenResponse _tokenResponse;
+  String _token;
   String name;
   String surname;
   String mail;
   String phone;
+  String text;
+
+  _sendRequestPostBodyHeadersKEY() async {
+    try {
+      var response = await http.post(_url,
+          body: jsonEncode({
+            "firstName": "test",
+            "lastName": "test",
+            "phone": "88005553535",
+            "email": "test@example.com"
+          }),
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json",
+          });
+
+      _status = response.statusCode;
+      _body = response.body;
+      //print("body:  ${response.body}");
+      _tokenResponse = TokenResponse.fromJson(jsonDecode(_body));
+      _token = _tokenResponse.data;
+    } catch (error) {
+      _status = 0;
+      print("error: $error");
+      _body = error.toString();
+    }
+    setState(() {}); //reBuildWidget
+    return _status;
+  } //_sendRequestPos
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,9 +84,7 @@ class PreRegistrationScreenState extends State<PreRegistrationScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Имя'
-                      ),
+                      decoration: InputDecoration(labelText: 'Имя'),
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Введите имя';
@@ -60,9 +95,7 @@ class PreRegistrationScreenState extends State<PreRegistrationScreen> {
                       },
                     ),
                     TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Фамилия'
-                      ),
+                      decoration: InputDecoration(labelText: 'Фамилия'),
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Введите фамилию';
@@ -73,9 +106,7 @@ class PreRegistrationScreenState extends State<PreRegistrationScreen> {
                       },
                     ),
                     TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'e-mail'
-                      ),
+                      decoration: InputDecoration(labelText: 'e-mail'),
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Введите e-mail';
@@ -86,9 +117,7 @@ class PreRegistrationScreenState extends State<PreRegistrationScreen> {
                       },
                     ),
                     TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Телефон'
-                      ),
+                      decoration: InputDecoration(labelText: 'Телефон'),
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Введите номер телефона';
@@ -98,17 +127,23 @@ class PreRegistrationScreenState extends State<PreRegistrationScreen> {
                         return null;
                       },
                     ),
+                    Text(text == null ? 'no' : text.toString()),
                   ])),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 50.0),
             child: RaisedButton(
               onPressed: () {
                 if (_formState.currentState.validate()) {
-                  Navigator.pushNamed(
+                  var code = _sendRequestPostBodyHeadersKEY();
+                  print(_token);
+                  text = _token;
+                  print("token: $_token");
+                  _formState.currentState.save();
+                  /*Navigator.pushNamed(
                     context,
                     RegistrationScreen.routeName,
-                    arguments: ScreenArguments(name, surname, mail, phone),
-                  );
+                    arguments: ScreenArguments(name, surname, mail, phone, _token),
+                  );*/
                 }
               },
               child: Text('ПОЛУЧИТЬ КЛЮЧ'),
@@ -138,6 +173,7 @@ class RegistrationScreenState extends State<RegistrationScreen> {
     String surname = args.name;
     String mail = args.mail;
     String phone = args.phone;
+    String token;
     String github;
     String resume;
 
@@ -154,9 +190,8 @@ class RegistrationScreenState extends State<RegistrationScreen> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Ссылка на github'
-                      ),
+                      decoration:
+                          InputDecoration(labelText: 'Ссылка на github'),
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Введите сслыку на github';
@@ -167,9 +202,8 @@ class RegistrationScreenState extends State<RegistrationScreen> {
                       },
                     ),
                     TextFormField(
-                      decoration: InputDecoration(
-                          labelText: 'Ссылка на резюие'
-                      ),
+                      decoration:
+                          InputDecoration(labelText: 'Ссылка на резюие'),
                       validator: (value) {
                         if (value.isEmpty) {
                           return 'Введите сслыку на резюие';
@@ -179,7 +213,7 @@ class RegistrationScreenState extends State<RegistrationScreen> {
                         return null;
                       },
                     ),
-
+                    Text(token),
                   ])),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 50.0),
@@ -202,11 +236,120 @@ class RegistrationScreenState extends State<RegistrationScreen> {
   }
 }
 
-class ScreenArguments {
-  final String name;
-  final String surname;
-  final String mail;
-  final String phone;
+class TestHttp extends StatefulWidget {
+  final String url;
 
-  ScreenArguments(this.name, this.surname, this.mail, this.phone);
+  TestHttp({String url}) : url = url;
+
+  @override
+  State<StatefulWidget> createState() => TestHttpState();
+}
+
+class TestHttpState extends State<TestHttp> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _url, _body;
+  int _status;
+  TokenResponse _token;
+
+  @override
+  void initState() {
+    _url = widget.url;
+    super.initState();
+  } //initState
+
+  _sendRequestPostBodyHeadersKEY() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save(); //update form data
+
+      try {
+        var response = await http.post(_url,
+            body: jsonEncode({
+              "firstName": "test",
+              "lastName": "test",
+              "phone": "88005553535",
+              "email": "test@example.com"
+            }),
+            headers: {
+              "content-type": "application/json",
+              "accept": "application/json",
+            });
+
+        _status = response.statusCode;
+        _body = response.body;
+        _token = TokenResponse.fromJson(jsonDecode(_body));
+      } catch (error) {
+        _status = 0;
+        _body = error.toString();
+      }
+      setState(() {}); //reBuildWidget
+    }
+  } //_sendRequestPos
+
+  _sendRequestPostSummary() async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save(); //update form data
+
+      try {
+        var response = await http.post(_url,
+            body: jsonEncode({
+              "firstName": "test",
+              "lastName": "test",
+              "phone": "88005553535",
+              "email": "test@example.com"
+            }),
+            headers: {
+              "content-type": "application/json",
+              "accept": "application/json",
+            });
+
+        _status = response.statusCode;
+        _body = response.body;
+        _token = TokenResponse.fromJson(jsonDecode(_body));
+      } catch (error) {
+        _status = 0;
+        _body = error.toString();
+      }
+      setState(() {}); //reBuildWidget
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            Container(
+                child: Text('API url',
+                    style: TextStyle(fontSize: 20.0, color: Colors.blue)),
+                padding: EdgeInsets.all(10.0)),
+            Container(
+                child: TextFormField(
+                    initialValue: _url,
+                    // ignore: missing_return
+                    validator: (value) {
+                      if (value.isEmpty) return 'API url isEmpty';
+                    },
+                    onSaved: (value) {
+                      _url = value;
+                    },
+                    autovalidate: true),
+                padding: EdgeInsets.all(10.0)),
+            SizedBox(height: 20.0),
+            RaisedButton(
+                child: Text('Send request POST to get '),
+                onPressed: _sendRequestPostBodyHeadersKEY),
+            SizedBox(height: 20.0),
+            Text('Response status',
+                style: TextStyle(fontSize: 20.0, color: Colors.blue)),
+            Text(_status == null ? '' : _status.toString()),
+            SizedBox(height: 20.0),
+            Text('Response body',
+                style: TextStyle(fontSize: 20.0, color: Colors.blue)),
+            //Text(_body == null ? '' : _body),
+            Text(_body == null ? '' : _token.data),
+          ],
+        )));
+  } //build
 }
